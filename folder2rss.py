b37b550f745c8e8ed98d31290c9e133e331ca2cf
Mattia Di Eleuterio <madiele92@gmail.com>
2023-05-13 18:12:53 +0200

    add thumbs

--- folder2rss.py
+++ folder2rss.py
@@ -63,7 +63,6 @@ class RSSRequestHandler(SimpleHTTPRequestHandler):
             parsed_path = parsed_path._replace(path=parsed_path.path[len("/" + config['subfolder']):])
         if parsed_path.path.endswith(".rss"):
             folder_name = unquote(parsed_path.path[:-4]).lstrip('/')
-            logging.info(folder_name)
             self.send_rss_feed(folder_name)
         else:
             super().do_GET()
@@ -71,15 +70,12 @@ class RSSRequestHandler(SimpleHTTPRequestHandler):
     def send_rss_feed(self, folder_name):
         base_folder = os.path.join(os.getcwd(), config['directory'])
         folder_path = os.path.join(base_folder, folder_name)
-        logging.info("folder_path: %s", folder_path)
         if os.path.isdir(folder_path):
             metadata_file = os.path.join(folder_path, "metadata.json")
-            logging.info("metadata_file: %s", metadata_file)
             metadata = {}
             if os.path.exists(metadata_file):
                 with open(metadata_file) as f:
                     metadata = json.load(f)
-            logging.info("metadata: %s", metadata)
             rss = self.create_rss_feed(folder_name, folder_path, metadata)
             self.send_response(200)
             self.send_header("Content-Type", "application/xml")
@@ -94,10 +90,11 @@ class RSSRequestHandler(SimpleHTTPRequestHandler):
         SubElement(channel, "itunes:block").text = "yes"
         SubElement(channel, "title").text = metadata.get("title", folder_name)
         SubElement(channel, "link").text = self.get_podcast_link(folder_name)
+        SubElement(channel, "itunes:image", href=self.get_pod_thumb_link(folder_name))
         SubElement(channel, "description").text = metadata.get("description", "")
 
         for filename in sorted(os.listdir(folder_path)):
-            if not filename.endswith(".jpg"):
+            if not filename.endswith(".jpg") and not filename.endswith(".json") and not filename.endswith(".png"):
                 episode_path = os.path.join(folder_path, filename)
                 if os.path.isfile(episode_path):
                     item = self.create_rss_item(folder_name, episode_path, filename)
@@ -119,7 +116,18 @@ class RSSRequestHandler(SimpleHTTPRequestHandler):
             "type": mimetypes.guess_type(filename)[0] or "application/octet-stream",
             "length": str(os.path.getsize(episode_path))
         })
+        SubElement(item, "thumbnail").text = self.get_episode_thumb_link(folder_name, filename)
         return item
+
+    def get_pod_thumb_link(self, folder_name):
+        base_url = self.get_website_root()
+        file_url = urljoin(base_url, f"{folder_name}/thumbnail.png")
+        return file_url
+
+    def get_episode_thumb_link(self, folder_name, filename):
+        base_url = self.get_website_root()
+        file_url = urljoin(base_url, f"{folder_name}/{os.path.splitext(filename)[0]}.jpg")
+        return file_url
 
     def get_episode_link(self, folder_name, filename):
         base_url = self.get_website_root()
